14f02ca37f9c773a65fc1f6173b6e26c75973faa
EmmaMcCaw <1020368@student.sae.edu.au>
2021-06-21 14:58:16 +1000

    Added sprites and points system

---
+++ Assets/Scripts/PointsSystemAC.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PointsSystemAC : MonoBehaviour
+{
+    public int scoreAmount;
+    public Text score; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreAmount = 0;
+       // score = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = scoreAmount.ToString();
+    }
+
+    public void AddScore()
+    {
+        scoreAmount += 1; 
+    }
+
+}
